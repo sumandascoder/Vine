16dc0081d55b6bb9425a50587f1d96875105b95d
sumansucharitdas <sumansucharitdas@sumans-mbp.home>
2015-02-06 16:37:42 -0500

    Adding Helper and Settings functionality on FragmentActivity screen of ListView.

--- src/com/viacom/ui/ListViewerActivity.java
+++ src/com/viacom/ui/ListViewerActivity.java
@@ -2,8 +2,14 @@ package com.viacom.ui;
 
 import com.example.myvine.R;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle; 
 import android.support.v4.app.FragmentActivity;
+import android.view.ContextThemeWrapper;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Container to hold the Fragments, ListView and Detail Views
@@ -29,5 +35,36 @@ public class ListViewerActivity extends FragmentActivity implements FragmentComm
 		detailFragment.receiveMsg(id);
 	}
 
-	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			Toast.makeText(this, "No settings Required", Toast.LENGTH_LONG).show();;
+			return true;
+		}
+		else if(id == R.id.help){
+			/**
+			 * New User help
+			 */
+			AlertDialog.Builder helpAlert =  new AlertDialog.Builder(new ContextThemeWrapper(this,
+					R.style.theme_dialog));
+			helpAlert.setTitle("I am a helper")
+				.setMessage("Provide the user link you want to view the content for and click "
+						+ "watch all the uploaded user videos.")
+				.setIcon(R.drawable.ic_launcher)
+				.setCancelable(true)
+				.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.cancel();
+					}
+				});
+			helpAlert.create().show();
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
